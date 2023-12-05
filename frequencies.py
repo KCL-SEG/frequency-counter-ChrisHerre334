c92cad26871db32a3ecd6c2ebb131a771cfa1b2d
ChrisHerre334 <147065977+ChrisHerre334@users.noreply.github.com>
2023-12-05 05:10:35 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    # Iterate through items
+    for item in items:
+        # Deal with non-string items
+        if not isinstance(item, str):
+            key = str(item)
+
+        # Look to next key and add current key to dictionary
+        frequencies[key] = frequencies.get(key, 0) + 1
+
+    # Return dictionary
     return frequencies
